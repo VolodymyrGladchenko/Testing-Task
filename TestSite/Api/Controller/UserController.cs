5baec3c55bd8eba07b621491bddd22615d517920
Volodymyr Gladchenko <volodymyr.gladchenko@gmail.com>
2015-10-12 00:13:29 +0300

    Add test site project

---
+++ TestSite/Api/Controller/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Api.Controller
+{
+    public class UserController : ApiController
+    {
+        [Route("api/users/{userId}")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TestSite/Contracts/Contracts/Request/GetUsersRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Contracts.Request
+{
+    
+    public class GetUsersRequest
+    {
+        public string UserId
+        {
+            get; set; 
+            
+        }
+    }
+}
